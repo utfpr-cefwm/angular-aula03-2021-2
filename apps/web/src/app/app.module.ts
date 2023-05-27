0845e174eb77f7fbc9322c4327abed99f3ead4ec
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-05-27 12:54:37 +0000

    Configura o módulo de roteamento raiz do app.
    - Comando:
      npx nx generate @schematics/angular:module app-routing --flat --no-interactive
    - Após criar o AppRoutingModule, é necessário fazer o restante das configurações manualmente.

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [
   ],
