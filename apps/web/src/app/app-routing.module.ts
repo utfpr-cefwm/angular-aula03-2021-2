6b0a5610976142cc6fe8ccc972c9b416dce9e7de
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-05-27 13:29:26 +0000

    Cria o HomeComponent.
    - Comando:
      npx nx generate @schematics/angular:component components/home --no-in
    teractive
    - Após isso, configuramos sua rota juntamente com um redirect padrão.

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -4,7 +4,18 @@ import {
   Routes,
 } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
