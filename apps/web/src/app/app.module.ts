ad0215209a17882d52d892fc105ca16c60b8e092
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-06-11 12:50:23 +0000

    Instala o `Toast` do PrimeNg.

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MenubarModule } from 'primeng/menubar';
 import { TabViewModule } from 'primeng/tabview';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,14 +23,17 @@ import { ListaArtigosComponent } from './components/lista-artigos/lista-artigos.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     MenubarModule,
     TabViewModule,
     CardModule,
     ButtonModule,
+    ToastModule,
   ],
   providers: [
+    MessageService,
     {
       provide: 'API_BASE_URL',
       useValue: 'http://localhost:4200/api',
